df6d04e3040bd6b120a926a878d3fec0caa262ac
zinface <zinface@163.com>
2023-11-03 00:20:14 +0800

    repo: 添加 6 个 fetch 页面

---
+++ pages/5_Fetch_Car_wallpaper.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import requests
+import io
+from PIL import Image
+import time
+
+def _max_width_(prcnt_width:int = 75):
+    max_width_str = f"max-width: {prcnt_width}rem;"
+    st.markdown(f""" 
+                <style> 
+                .block-container{{{max_width_str}}}
+                </style>    
+                """, 
+                unsafe_allow_html=True,
+    )
+_max_width_(80)
+
+st.button('Fetch Car wallpaper')
+
+
+def getImageLink():
+    return 'https://api.gumengya.com/Api/QcImg?format=image'
+
+def getImage(url):
+    return requests.get(url).content
+
+def getDogeBytesIO():
+    container = io.BytesIO()
+    container.write(getImage(getImageLink()))
+    return container
+
+loading_bar = st.progress(0.0, '加载中')
+time.sleep(0.1)
+loading_bar.progress(0.1, '加载中')
+image = getDogeBytesIO()
+loading_bar.progress(1.0, '加载完成')
+time.sleep(0.1)
+loading_bar.image(image)
---
+++ welcome.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+
+st.title('Welcome Streamlit MainPage')
